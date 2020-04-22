c4a781de9381d6c7b5edc870324084c00a9dfc7b
NoussDev <yannisleiseing@protonmail.com>
2020-04-22 17:39:21 +0200

    I added api connexion, after you logged in you arrive on the dashboard page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 import PageWrapper from './components/PageWrapper'
+import { connect } from 'react-redux'
 
 // Pages
 import Home from './components/Pages/Home'
@@ -9,50 +10,74 @@ import About from './components/Pages/About'
 import Contact from './components/Pages/Contact'
 import AdminWrapper from './components/AdminWrapper';
 import Login from './components/Pages/Login';
+import DashBoard from './components/Pages/DashBoard';
 
 
-function App() {
-  return (
-    <Router>
-
-      <Route
-        path="/admin"
-        render={props => (
-          <AdminWrapper>
-            <Login />
-          </AdminWrapper>
-        )}
-      />
-      <Route
-        exact
-        path="/"
-        render={props => (
-          <PageWrapper>
-            <Home {...props} />
-          </PageWrapper>
-        )}
-      />
-
-      <Route
-        path="/about"
-        render={props => (
-          <PageWrapper>
-            <About {...props} />
-          </PageWrapper>
-        )}
-      />
-
-      <Route
-        path="/contact"
-        render={props => (
-          <PageWrapper>
-            <Contact {...props} />
-          </PageWrapper>
-        )}
-      />
-    </Router>
-
-  );
+class App extends Component {
+  render(){
+    return (
+      <Router>
+  
+        <Route
+          path="/admin"
+          render={props => {
+            return(
+              <AdminWrapper>
+              {this.props.auth.token ?
+                <DashBoard />
+                :
+                <Login />
+              }
+            </AdminWrapper>
+            )}
+          }         
+        />
+        <Route
+          exact
+          path="/"
+          render={props => (
+            <PageWrapper>
+              <Home {...props} />
+            </PageWrapper>
+          )}
+        />
+  
+        <Route
+          path="/about"
+          render={props => (
+            <PageWrapper>
+              <About {...props} />
+            </PageWrapper>
+          )}
+        />
+  
+        <Route
+          path="/contact"
+          render={props => (
+            <PageWrapper>
+              <Contact {...props} />
+            </PageWrapper>
+          )}
+        />
+      </Router>
+  
+    )
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return{
+    auth: state.auth
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -1,10 +1,18 @@
+import API from "../utils/api"
+
 export const login = (email, password) => {
-    return{
-        type: 'LOGIN',
-        payload: {
-            email,
-            password
-        }
+    return (dispatch) => {
+        API.login(email,password,res => {
+            console.log("Result : ", res.data)
+            dispatch({
+                type: 'LOGIN',
+                payload: {
+                    email: email,
+                    token: res.data.id,
+                    userId: res.data.userId
+                }
+            })
+        })
     }
 }
 
